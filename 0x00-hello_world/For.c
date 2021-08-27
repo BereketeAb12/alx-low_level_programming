b4bfa58f478dbf4f864e3d4ae8cee092b234923f
BereketeAb Birku <BereketeabBirkuk@gmail.com>
2021-08-27 23:16:38 +0300

    c

---
+++ 0x00-hello_world/For.c
@@ -0,0 +1,36 @@
+int a;
+int b;
+
+/*
+if (a > b)
+{
+  return (a);
+}
+
+ 
+if {a > b}
+(
+  return {a};
+)
+
+if ((((((a > b))))))
+{
+  return (a);
+}
+
+
+if (a > b)
+  return (a);
+
+*/
+
+
+
+int a;
+int b = 3;
+
+for (int a = 0; a < b; a++)
+{
+    printf("%d\n", a);
+}
+
