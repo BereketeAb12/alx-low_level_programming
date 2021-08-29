b0421a4ab45ed53e73794146904906f19a2563b8
BereketeAb Birku <BereketeabBirkuk@gmail.com>
2021-08-30 01:15:47 +0300

    lower

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing a lowercase
+ *
+ * @char - obtaining a letter
+ *
+ * Return: return the letter
+ */
+
+int main()
+{
+char = 'a';
+putchar(char'\n');
+return(0);
+}
