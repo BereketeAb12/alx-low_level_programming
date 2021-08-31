0bf7c14a00b29b9484801364093dc8f383fce50e
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-08-31 13:04:52 +0200

    MAARTEN

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-write (1,%s%s "%"Programming is like building a multilingual puzzle", 13)
+#include <stdio.h>
+/**
+ *main - mmain
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+put("Programming is like building a multilingual puzzle");
+return (0);
+}
