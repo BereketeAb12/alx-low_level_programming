2190b5d4dbe2378be9d3f35c77090c19830d2527
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-09-01 23:47:07 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
  * main - printing size
- * 
+ *
  * Description: DAtaTyPe
  *
  * Return: Zero again
  */
-int main()
+int main(void)
 {
-  char Character;
-  int Integer;
-  long int LongInt;
-  long long int LongLongInt;
-  float floating;
+char Character;
+int Integer;
+long int LongInt;
+long long int LongLongInt;
+float floating;
 
 printf("Size of a char: %d\n", sizeof(Character));
 printf("Size of a int: %d\n", sizeof(Integer));
 printf("Size of a long int: %d\n", sizeof(LongInt));
 printf("Size of a long long int: %d\n", sizeof(LongLongInt));
 printf("Size of a float: %d\n", sizeof(floating));
-return (0)
+return (0);
 }
