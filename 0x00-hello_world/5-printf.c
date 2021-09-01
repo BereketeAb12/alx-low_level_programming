9716d37b8ead50c17877fccf8eda69d3f36e259c
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-09-01 23:31:33 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#include stdio.h
-printf("with proper grammar, but the outcome is a piece of art,\n")
+#include <stdio.h>
+/**
+ * main - returning 0
+ *
+ * Description: printsf
+ *
+ * Return: zero
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
