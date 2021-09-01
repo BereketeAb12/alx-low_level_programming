5de84a5e00b7b1d4888acc5d990ee5c6bb0c6b4e
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-09-01 23:48:49 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ long int LongInt;
 long long int LongLongInt;
 float floating;
 
-printf("Size of a char: %d\n", sizeof(Character));
-printf("Size of a int: %d\n", sizeof(Integer));
-printf("Size of a long int: %d\n", sizeof(LongInt));
-printf("Size of a long long int: %d\n", sizeof(LongLongInt));
-printf("Size of a float: %d\n", sizeof(floating));
+printf("Size of a char: %d byte(s)\n", sizeof(Character));
+printf("Size of a int: %d byte(s)\n", sizeof(Integer));
+printf("Size of a long int: %d byte(s)\n", sizeof(LongInt));
+printf("Size of a long long int: %d byte(s)\n", sizeof(LongLongInt));
+printf("Size of a float: %d\n byte(s)", sizeof(floating));
 return (0);
 }
