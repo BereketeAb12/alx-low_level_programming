34d3a194f56a1944a5a5a0b7bdfc1f162f926c8b
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-09-02 01:48:49 +0300

    wohooooo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printing a #
+ *
+ * Description: letter numbers printing
+ *
+ * Return: return the letter
+ */
+
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
