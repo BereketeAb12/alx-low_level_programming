e613042027be593f0147b0966d6d2f5cc24a21a7
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-09-02 01:25:33 +0300

    getting used to it

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing a #
+ *
+ * Description: letter numbers printing
+ *
+ * Return: return the letter
+ */
+
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
