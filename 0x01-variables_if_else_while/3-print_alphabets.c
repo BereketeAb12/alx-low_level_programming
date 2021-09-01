e4f43c49f0349e910878cc8a3e759cc1bfe2b670
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-09-02 00:37:53 +0300

    Certain

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - printing a lowercase
+ *
+ * Description: letter array printing
+ *
+ * Return: return the letter
+ */
+
+int main(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (x = 'A'; x <= 'Z'; x++)
+{
+putchar (x);
+}
+putchar('\n');
+return (0);
+}
