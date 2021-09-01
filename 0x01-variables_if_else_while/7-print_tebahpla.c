fb8ff079c102c35e2aef9321e2cfc0c18c498445
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-09-02 01:36:55 +0300

    myWork

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- reverse
+ *
+ * Description: make it reverse
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+
+for (int i = 'z'; i >= 'a'; i--)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
+
