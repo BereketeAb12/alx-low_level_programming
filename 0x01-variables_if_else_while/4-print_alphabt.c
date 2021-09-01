8a5e785af71998d8296f9a49570d7a7358d274c3
BereketeAb <BereketeAbbirkuk@gmail.com>
2021-09-02 01:09:48 +0300

    Succesfully skipped e and q with continue function

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - printing a lowercase
+ *
+ * Description: letter array printing
+ *
+ * Return: return the letter
+ */
+
+int main(void)
+{
+int i;
+ for (i = 'a'; i <= 'z'; i++;)
+{
+if(i == 'e')
+continue;
+if(i == 'q')
+continue;
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
