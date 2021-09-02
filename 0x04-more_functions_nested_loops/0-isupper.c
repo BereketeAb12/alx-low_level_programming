ad11f377dbf698d113e38820b43ae41d22a47aaf
BereketeAb Birku <BereketeabBirkuk@gmail.com>
2021-09-03 00:17:00 +0300

    esti

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main- checking uppercase
+ *
+ * Description: check Check
+ *
+ * Return: 0 or 1
+ */
+int main(void)
+{
+int c = 'A';
+if (isupper(c))
+{
+return (1);
+}
+else
+return (0);
+}
